afcc293d103bb48c641ef0e6ac7bae2b04d15824
Sranjan20 <sudhranjan123@gmail.com>
2021-12-14 23:37:47 +0530

    light to dark

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class SarvasvaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
+      theme: ThemeData.dark(),
       home: const OpeningPage(),
     );
   }
